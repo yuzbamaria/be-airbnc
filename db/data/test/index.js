f6144347ac4b7737bb1ef132f963727022693fb6
Mariia Yuzba <54204342+yuzbamaria@users.noreply.github.com>
2024-11-25 19:21:22 +0000

    Initial commit

---
+++ db/data/test/index.js
@@ -0,0 +1,7 @@
+exports.bookingsData = require("./bookings.json");
+exports.favouritesData = require("./favourites.json");
+exports.imagesData = require("./images.json");
+exports.propertiesData = require("./properties.json");
+exports.propertyTypesData = require("./property-types.json");
+exports.reviewsData = require("./reviews.json");
+exports.usersData = require("./users.json");
