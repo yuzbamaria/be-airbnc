152b4d09e4a949cd0f28f427ecee417102d7c3bd
Maria Yuzba <mariayuzba@Maria.local>
2025-01-05 23:39:59 +0000

    added description of 1 endpoint and it's styling for static html

--- routes/allEndpointes.router.js
+++
@@ -1,9 +0,0 @@
-// const express = require("express");
-// const allEndpointsRouter = express.Router();
-
-// const { getAllEndpoints } = require("../controllers/allEndpointsController");
-
-// allEndpointsRouter
-//     .get("/", getAllEndpoints);
-
-// module.exports = allEndpointsRouter;
