e7a03561662e8796095b4463f9ef38e7691fa688
Maria Yuzba <mariayuzba@Maria.local>
2025-02-25 19:48:26 +0000

    updated fetchProperty model

--- models/propertiesModels.js
+++ models/propertiesModels.js
@@ -76,7 +76,6 @@ exports.fetchProperties = async(sort = "popularity", order = "desc", host, maxpr
 
     return { properties: orderedProperties };
 };
-// this.fetchProperties("popularity", "desc", 1, 500, 100).then((result) => console.log(result.properties.length))
 
 async function fetchFavouriteByUser(property_id, guest_id) {
     const queryStr = `
@@ -87,21 +86,8 @@ async function fetchFavouriteByUser(property_id, guest_id) {
     return rows;
 };
 
-async function fetchAllImagesById(property_id) {
-    const queryStr = `
-        SELECT 
-            images.image_url as image
-        FROM properties
-        JOIN users ON 
-            properties.host_id = users.user_id
-        JOIN images ON 
-            properties.property_id = images.property_id
-        WHERE properties.property_id = $1;`
-    const { rows } = await db.query(queryStr, [property_id]);
-    return rows;
-};
-
-exports.fetchProperty = async(property_id, user_id) => {
+exports.fetchProperty = async (property_id, user_id) => {
+  try {
     let queryStr = `
         SELECT
             properties.property_id,
@@ -112,7 +98,7 @@ exports.fetchProperty = async(property_id, user_id) => {
             CONCAT(users.first_name, ' ', users.surname) as host,
             users.avatar as host_avatar,
             COUNT(favourites.favourite_id) as favourite_count,
-            images.image_url as images
+            ARRAY_AGG(images.image_url) as images
         FROM properties
         JOIN users ON 
             properties.host_id = users.user_id
@@ -120,42 +106,31 @@ exports.fetchProperty = async(property_id, user_id) => {
             properties.property_id = favourites.property_id
         JOIN images ON 
             properties.property_id = images.property_id
-        WHERE properties.property_id = $1`;
-    
-    let favourited;
+        WHERE properties.property_id = $1
+        GROUP BY 
+            properties.property_id, 
+            users.first_name, 
+            users.surname, 
+            users.avatar;`;
+
     const params = [property_id];
 
-    if(user_id) {
-        queryStr += ` AND favourites.guest_id = $2`;
-        params.push(user_id);
-        const favourites = await fetchFavouriteByUser(property_id, user_id);
-        if (favourites.length > 0) {
-            favourited = true;
-        };
-    };
-
-    queryStr += `GROUP BY 
-        properties.property_id, 
-        users.first_name, 
-        users.surname, 
-        users.avatar,
-        images.image_url;`;
-
     const { rows } = await db.query(queryStr, params);
-    if(rows.length === 0) {
-        return Promise.reject({ status: 404, msg: "Resource not found." })
-    };
-    
+    if (rows.length === 0) {
+      return Promise.reject({ status: 404, msg: "Resource not found." });
+    }
+
     const property = rows[0];
     if (user_id) {
-        property.favourited = favourited;
-    };
-    
-    const allImages = await fetchAllImagesById(property_id);
-    property.images = allImages;
- 
-    return { property: property };
-};
+        const favourites = await fetchFavouriteByUser(property_id, user_id);
+        property.favourited = favourites.length > 0;
+      }
+      return { property };
+    } catch (err) {
+      console.error(err);
+    //   return Promise.reject({ status: 500, msg: "Internal server error." });
+    }
+  };
 
 
 
