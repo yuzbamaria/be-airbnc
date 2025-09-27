28458e833579a9e16ea145a0cf6d73c9c04bc946
Maria Yuzba <yuzba.maria@gmail.com>
2025-09-27 16:09:43 +0100

    fix: update review router to contain addReview

--- routes/reviews.router.js
+++ routes/reviews.router.js
@@ -3,9 +3,9 @@ const reviewsRouter = express.Router();
 const authMiddleware = require("../middleware/authMiddleware");
 const checkRole = require("../middleware/checkRole");
 
-const { deleteReview } = require("../controllers/reviewsController");
+const { addReview, deleteReview } = require("../controllers/reviewsController");
 
-reviewsRouter.post("/", authMiddleware, checkRole("guest"), createReview);
+reviewsRouter.post("/", authMiddleware, checkRole("guest"), addReview);
 reviewsRouter.delete("/:id", authMiddleware, checkRole("guest"), deleteReview);
 
 module.exports = reviewsRouter;
